1958e379d7fd9867936dd4d3c287a9f6fadafc30
Vic Iglesias <viglesias@google.com>
2016-10-19 14:25:53 -0700

    Install logging and monitoring agents on Redis

--- scripts/redis.sh
+++ scripts/redis.sh
@@ -13,6 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Install monitoring and logging agents
+curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+bash install-logging-agent.sh
+curl -sSO https://repo.stackdriver.com/stack-install.sh
+bash stack-install.sh --write-gcm
+
 apt-get update
 apt-get install -y redis-server
 sed -i 's#^bind.*#bind 0.0.0.0#g' /etc/redis/redis.conf
